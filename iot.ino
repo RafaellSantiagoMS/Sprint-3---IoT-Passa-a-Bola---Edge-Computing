d7d157f5cf2ad46a63faf24b245496be72f25116
Rafaell <rafaellsantiagoms@gmail.com>
2025-09-15 21:40:35 -0300

    feat: adicionado configurações wifi e thingspeak

--- iot.ino
+++ iot.ino
@@ -4,3 +4,11 @@
 #include <Wire.h>
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
+
+// ---------- CONFIGURAÇÃO WIFI ----------
+const char* WIFI_SSID = "Wokwi-GUEST";
+const char* WIFI_PASSWORD = "";
+
+// ---------- CONFIGURAÇÃO THINGSPEAK ----------
+const char* THINGSPEAK_HOST = "http://api.thingspeak.com/update";
+const char* THINGSPEAK_API_KEY = "30TUBORX1EBYN3AO"; // sua chave
