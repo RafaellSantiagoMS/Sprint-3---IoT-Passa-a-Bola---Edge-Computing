003c9e7dfcbbdecd781fe074ff68cb40467b91ec
Rafaell <rafaellsantiagoms@gmail.com>
2025-09-15 21:42:00 -0300

    feat: adicionado configuração dos pinos

--- iot.ino
+++ iot.ino
@@ -11,4 +11,12 @@ const char* WIFI_PASSWORD = "";
 
 // ---------- CONFIGURAÇÃO THINGSPEAK ----------
 const char* THINGSPEAK_HOST = "http://api.thingspeak.com/update";
-const char* THINGSPEAK_API_KEY = "30TUBORX1EBYN3AO"; // sua chave
+const char* THINGSPEAK_API_KEY = "30TUBORX1EBYN3AO"; 
+
+
+// ---------- PINOS DOS POTENCIÔMETROS ----------
+const int pinBatimentos = 34; // simula batimentos cardíacos
+const int pinOximetro = 35;   // simula oximetria
+
+// ---------- OBJETO MPU6050 ----------
+Adafruit_MPU6050 mpu;
